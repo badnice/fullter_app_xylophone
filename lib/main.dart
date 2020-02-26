4c7d4fa3a03a7d3a06d76254765adb185b1bedc6
badis <barkat.badis@hotmail.fr>
2020-02-26 14:51:22 +0100

    creation fonction

--- lib/main.dart
+++ lib/main.dart
@@ -6,106 +6,106 @@ void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
   @override
+  
+  void player(int number) {
+    final player = AudioCache();
+    player.play('note$number.wave');
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Center(
-          child: Text(
-            'Xylophone',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-              letterSpacing: 1,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Center(
+            child: Text(
+              'Xylophone',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                letterSpacing: 1,
+              ),
             ),
           ),
         ),
-      ),
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Expanded(
-              child: FlatButton(
-                child: Text('do'),
-                color: Colors.red,
-                onPressed: () {
-                  print('note1');
-                  final player = AudioCache();
-                  player.play('note1.wav');
-                },
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                    child: Text('do'),
+                    color: Colors.red,
+                    onPressed: () {
+                      print('note');
+
+                      player(1);
+                    }),
               ),
-            ),
-            Expanded(
-              child: FlatButton(
-                child: Text('ré'),
-                color: Colors.orange,
-                onPressed: () {
-                  print('note2');
-                  final player = AudioCache();
-                  player.play('note2.wav');
-                },
+              Expanded(
+                child: FlatButton(
+                  child: Text('ré'),
+                  color: Colors.orange,
+                  onPressed: () {
+                    print('note2');
+                    player(2);
+                  },
+                ),
               ),
-            ),
-            Expanded(
-              child: FlatButton(
-                child: Text('mi'),
-                color: Colors.yellow,
-                onPressed: () {
-                  print('note3');
-                  final player = AudioCache();
-                  player.play('note3.wav');
-                },
+              Expanded(
+                child: FlatButton(
+                  child: Text('mi'),
+                  color: Colors.yellow,
+                  onPressed: () {
+                    print('note3');
+                    player(3);
+                  },
+                ),
               ),
-            ),
-            Expanded(
-              child: FlatButton(
-                child: Text('fa'),
-                color: Colors.green,
-                onPressed: () {
-                  print('note4');
-                  final player = AudioCache();
-                  player.play('note4.wav');
-                },
+              Expanded(
+                child: FlatButton(
+                  child: Text('fa'),
+                  color: Colors.green,
+                  onPressed: () {
+                    print('note4');
+                    player(4);
+                  },
+                ),
               ),
-            ),
-            Expanded(
-              child: FlatButton(
-                child: Text('sol'),
-                color: Colors.green.shade900,
-                onPressed: () {
-                  print('note5');
-                  final player = AudioCache();
-                  player.play('note5.wav');
-                },
+              Expanded(
+                child: FlatButton(
+                  child: Text('sol'),
+                  color: Colors.green.shade900,
+                  onPressed: () {
+                    print('note5');
+                    player(5);
+                  },
+                ),
               ),
-            ),
-            Expanded(
-              child: FlatButton(
-                child: Text('la'),
-                color: Colors.blue,
-                onPressed: () {
-                  print('note6');
-                  final player = AudioCache();
-                  player.play('note6.wav');
-                },
+              Expanded(
+                child: FlatButton(
+                  child: Text('la'),
+                  color: Colors.blue,
+                  onPressed: () {
+                    print('note6');
+                    player(6);
+                  },
+                ),
               ),
-            ),
-            Expanded(
-              child: FlatButton(
-                child: Text('si'),
-                color: Colors.purple,
-                onPressed: () {
-                  print('note7');
-                  final player = AudioCache();
-                  player.play('note7.wav');
-                },
+              Expanded(
+                child: FlatButton(
+                  child: Text('si'),
+                  color: Colors.purple,
+                  onPressed: () {
+                    print('note7');
+                    player(7);
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
